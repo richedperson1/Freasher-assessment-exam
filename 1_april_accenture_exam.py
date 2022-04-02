2895da106e10cd0864821dddcf3c53fd49c33473
Rutvik Jaiswal <62417643+richedperson1@users.noreply.github.com>
2022-04-02 16:32:54 +0530

    Create 1_april_accenture_exam.py

---
+++ 1_april_accenture_exam.py
@@ -0,0 +1,35 @@
+# Frequency count of string 
+"""
+Input of string is to be lowercase letter only.
+
+
+input 1  : babdc
+output 1 : a1b1c1d1
+
+input 2  :  cbd
+output 2 : b1c1d1
+
+
+TC = O(n)
+SC = O(1)
+
+"""
+print("=-="*25)
+string = "cbd"
+
+counting_alph = [0]*26
+for word in string:
+    local = ord(word)- ord('a')
+    counting_alph[local]+=1
+
+form = ""
+for idx, count in enumerate(counting_alph):
+    if count:
+        local = chr(idx+97)
+        form+=(local+str(count))
+
+print(form)
+"""
+author : rutvikjaiswal195@gmail.com
+
+"""
